85032e58493b0be11cfc7e36ad5141190df10821
Kevin Bougeois <kevin.bourgeois@univ-orleans.fr>
2021-03-09 15:41:51 +0100

    test from yaml box

--- tests_tp2.py
+++ tests_tp2.py
@@ -1,5 +1,6 @@
 from tp2 import *
-
+import yaml
+import io
 def test_box_create():
     b = Box()
 
@@ -112,3 +113,22 @@ def test_find():
 
     b.close()
     assert b.find("bidule") is None
+
+def test_from_yaml():
+    text="""
+    - is_open: true
+      capacity: 3
+    - is_open: false
+      capacity: 5
+    """
+    stream = io.StringIO(text)
+    l = yaml.load(stream)
+    
+    b1 = Box(is_open=True, capacity=3)
+    b1_yaml = Box.from_yaml(l[0])
+
+    b2 = Box(is_open=False, capacity=5)
+    b2_yaml = Box.from_yaml(l[1])
+
+    assert b1 == b1.yaml
+    assert b2 == b2.yaml
